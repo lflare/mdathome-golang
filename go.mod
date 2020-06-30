cf53102a2858d6114d3d1c396b5697a33e12cf48
Amos Ng <amosng1@gmail.com>
2020-06-30 12:41:29 +0800

    Revert "Added rudimentary HTTP/3 QUIC support"
    This reverts commit 89051bd79d2d0e1f40211cc4b8eba7531eee75a7.

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gorilla/mux v1.7.4
 	github.com/hashicorp/go-retryablehttp v0.6.6
-	github.com/lflare/diskcache-golang v0.1.0
-	github.com/lucas-clemente/quic-go v0.17.1
+    github.com/lflare/diskcache-golang v0.1.0
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae
 )
--- tls.go
+++ tls.go
@@ -6,8 +6,6 @@ import (
 	"net"
 	"net/http"
 	"time"
-
-	"github.com/lucas-clemente/quic-go/http3"
 )
 
 type tcpKeepAliveListener struct {
@@ -24,76 +22,17 @@ func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
 	return tc, nil
 }
 
-func ListenAndServe(addr string, tlsConfig *tls.Config, handler http.Handler) error {
-	// Prepare listener transports
-	udpAddr, err := net.ResolveUDPAddr("udp", addr)
-	if err != nil {
-		return err
-	}
-	udpConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		return err
-	}
-	defer udpConn.Close()
-
-	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
-	if err != nil {
-		return err
-	}
-	tcpConn, err := net.ListenTCP("tcp", tcpAddr)
-	if err != nil {
-		return err
-	}
-	defer tcpConn.Close()
-
-	// Prepare TLS listener
-	tlsConn := tls.NewListener(tcpConn, tlsConfig)
-	defer tlsConn.Close()
-
-	// Prepare servers
-	httpServer := &http.Server{
-		Addr:      addr,
-		TLSConfig: tlsConfig,
-        ReadTimeout:  1 * time.Minute,
-        WriteTimeout: 1 * time.Minute,
-	}
-
-	quicServer := &http3.Server{
-		Server: httpServer,
-	}
-
-	// Prepare httpServer handler
-	httpServer.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		quicServer.SetQuicHeaders(w.Header())
-		handler.ServeHTTP(w, r)
-	})
-
-	// Create error handler channels
-	hErr := make(chan error)
-	qErr := make(chan error)
-	go func() {
-		hErr <- httpServer.Serve(tlsConn)
-	}()
-	go func() {
-		qErr <- quicServer.Serve(udpConn)
-	}()
-
-	// Synchronise and wait for errors
-	select {
-		case err := <-hErr:
-			quicServer.Close()
-			return err
-		case err := <-qErr:
-			return err
-	}
-}
-
 func ListenAndServeTLSKeyPair(addr string, cert tls.Certificate, handler http.Handler) error {
 	if addr == "" {
 		return errors.New("Invalid address string")
 	}
 
-	// Prepare TLS configuration
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      handler,
+		ReadTimeout:  1 * time.Minute,
+		WriteTimeout: 1 * time.Minute,
+	}
 	config := &tls.Config{
 		PreferServerCipherSuites: true,
 		CurvePreferences: []tls.CurveID{
@@ -105,6 +44,11 @@ func ListenAndServeTLSKeyPair(addr string, cert tls.Certificate, handler http.Ha
 	config.Certificates = make([]tls.Certificate, 1)
 	config.Certificates[0] = cert
 
-	// Prepare HTTP/3 server
-	return ListenAndServe(addr, config, handler)
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	tlsListener := tls.NewListener(tcpKeepAliveListener{ln.(*net.TCPListener)}, config)
+	return server.Serve(tlsListener)
 }
