431a1083cb6890c7f2eb9c29e93c37645a7c634e
Amos Ng <amosng1@gmail.com>
2021-01-25 23:28:57 +0800

    Fixed commentings

--- internal/mdathome/constants.go
+++ internal/mdathome/constants.go
@@ -1,8 +1,13 @@
 package mdathome
 
+////
 // Client settings
+///
+
+// ClientVersion is the description of the version of the working branch when compiled
 var ClientVersion string
 
+// ClientSpecification is the integer version of the official specification this client is supposed to work against
 const ClientSpecification int = 20
 
 // Backend settings (Swap to use testnet)
